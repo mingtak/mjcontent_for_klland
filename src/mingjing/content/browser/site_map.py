55edcfcc03aec21f75c18c1d595e8915499a1117
Andy Fang <andy@mingtak.com.tw>
2017-07-23 17:28:23 +0800

    add site_map

---
+++ src/mingjing/content/browser/site_map.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+from datetime import datetime
+from DateTime import DateTime
+import json
+#from plone.memoize import ram
+from time import time
+from Products.CMFPlone.utils import safe_unicode
+import logging
+import pickle
+import MySQLdb
+import operator
+
+
+logger = logging.getLogger('mingjing.content')
+
+
+class SiteMap(BrowserView):
+
+    template = ViewPageTemplateFile("template/site_map.pt")
+
+    def __call__(self):
+        request = self.request
+
+        return self.template()
